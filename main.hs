fe1b07fef1c488ad8a5d65e5871db7c687c01211
MEgor <eguar@tut.by>
2015-12-12 23:35:06 +0300

    matrixCellValue function is created

--- main.hs
+++ main.hs
@@ -10,6 +10,11 @@ import Data.Conduit
 
 import Data.Random.Extras
 
+m = 2
+centersList = [[1.0,2.0], [2.0,3.0]]
+currCenter = [1.0,2.0]
+currObject = [3.0,4.0]
+
 convertFromCsv :: V.Vector (Row String) -> [[Double]]
 convertFromCsv = processCsv . V.toList
     where processCsv = filter (not . null) . map processRow 
@@ -21,6 +26,16 @@ euclidDistance a b = sqrt (sum (zipWith (\a b -> (a - b)**2) a b))
 
 getInitialCenters :: [a] -> Int -> [a]
 getInitialCenters xs n = take n xs 
+ 
+matrixCellValueOnIteration :: [Double] -> [Double] -> [Double] -> Double
+matrixCellValueOnIteration centerFromList currCenter currObject = 
+    ((euclidDistance currObject currCenter) / (euclidDistance currObject centerFromList)) ** (2 / (m - 1))
+
+matrixCellValue :: [[Double]] -> [Double] -> [Double] -> Double
+matrixCellValue centersList currCenter currObject = 
+    (foldl (\acc centerFromList -> acc + matrixCellValueOnIteration centerFromList currCenter currObject) 0 centersList) ** (-1)
+
+
 
 
 main = do
